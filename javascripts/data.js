4880f7b49613997830117c8902f9399d358d8e69
Adam Wieckert <wiec1369@gmail.com>
2018-04-23 20:32:52 -0500

    Added the data setters and getters and successfunctions.

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,4 +1,25 @@
 let usersArray = [];
 let messagesArray = [];
 
-const usersArray = () => {};
+const setUsers = (userData) => {
+  usersArray = userData;
+};
+
+const getUsers  = () => {
+  return usersArray;
+};
+
+const setMessages = (messageData) => {
+  messagesArray = messageData;
+};
+
+const getMessages = () => {
+  return messagesArray;
+};
+
+module.exports = {
+  setUsers,
+  getUsers,
+  setMessages,
+  getMessages,
+};
--- javascripts/dataGate.js
+++ javascripts/dataGate.js
@@ -1,17 +1,17 @@
-const xhrs = require('./xhr');
+const xhrs = require('./xhr.js');
 const data = require('./data.js');
-const printToDom = require('./dom.js');
+// const printToDom = require('./dom.js');
 
 function successFunction1 () {
   const userData = JSON.parse(this.responseText).users;
   data.setUsers(userData);
-  xhr.xhr2(successFunction2, WTF);
+  xhrs.xhr2(successFunction2, WTF);
 };
 
 function successFunction2 () {
   const messageData = JSON.parse(this.responseText).messages;
   data.setMessages(messageData);
-  printToDom();
+  // printToDom();
 };
 
 function WTF () {
