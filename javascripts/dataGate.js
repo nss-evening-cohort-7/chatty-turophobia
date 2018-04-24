de89c7dd5dbe36ad8cc99c549f494b786ac30e48
Rita Patrick <rita.patrick1007@gmail.com>
2018-04-24 14:52:30 -0500

    dropdown with users prints and messages print with user names and edit and delete buttons, need more css/bootstrap for styling, bug - dropdown doesnt fit in nav

--- javascripts/dataGate.js
+++ javascripts/dataGate.js
@@ -1,17 +1,21 @@
 const xhrs = require('./xhr.js');
 const data = require('./data.js');
+const prints = require('./dom.js');
 // const printToDom = require('./dom.js');
 
 function successFunction1 () {
   const userData = JSON.parse(this.responseText).users;
   data.setUsers(userData);
   xhrs.xhr2(successFunction2, WTF);
+  prints.printUsers(userData);
 };
 
 function successFunction2 () {
   const messageData = JSON.parse(this.responseText).messages;
   data.setMessages(messageData);
+  prints.printMessages(data.getUsers(), messageData);
   // printToDom();
+
 };
 
 function WTF () {
---
+++ javascripts/dom.js
@@ -0,0 +1,46 @@
+const printToDom = (domString, divId) => {
+  document.getElementById(divId).innerHTML = domString;
+};
+
+const printUsers = usersArray => {
+  console.log(usersArray);
+  let domString = '';
+  // domString += `<div class="row">`;
+  // domString +=   `<div class="col-xs-6 col-md-2">`;
+  // domString +=     `<div class="form-group">`;
+  domString +=       `<select class="selectpicker form-control" title="Choose user...">`;
+  usersArray.forEach ((user) => {
+    domString +=         `<option>${user.userName}</option>`;
+  });
+  domString +=       `</select>`;
+  // domString +=     `</div>`;
+  // domString +=   `</div>`;
+  // domString += `</div>`;
+  printToDom(domString, 'users-output');
+};
+const printMessages = (users, messages) => {
+  let domString = '';
+  for (let i = 0; i < users.length; i++) {
+    domString += `<div class='well well-sm clearfix'>`;
+    // domString += `<div class="pull-left">`;
+    domString += `<h5>${users[i].userName}</h5>`;
+    domString += `<div class="pull-left">`;
+    for (let j = 0; j < messages.length; j++) {
+      if (users[i].id === messages[j].userId)
+        domString += `<p>${messages[i].message}</p>`;
+    }
+    domString += `</div>`;
+
+    domString += `<div class="pull-right">`;
+    domString += `<button class="btn btn-default" type="submit">Edit</button>`;
+    domString += `<button class="btn btn-default" type="submit">Delete</button>`;
+    domString += `</div>`;
+    domString += `</div>`;
+  }
+  printToDom(domString, 'messages-output');
+};
+
+module.exports = {
+  printUsers,
+  printMessages,
+};
