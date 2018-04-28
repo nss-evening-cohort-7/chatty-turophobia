241499ce7ab783dd43febb8ae9ed85a605ceced4
Drake Morrison <drakermorrison@gmail.com>
2018-04-28 13:23:06 -0500

    finished badwords function

--- javascripts/badWords.js
+++ javascripts/badWords.js
@@ -1,22 +1,12 @@
 const data = require('./data.js');
 
-// const regexEscape = (str) => {
-//   return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-// };
-
 const checkBadWords = (checkedMessage) => {
   const badWords = data.getBadWords();
   badWords.forEach((badWord) => {
     if (checkedMessage.includes(badWord)) {
-      // const re = /badWord/gi;
-      // const re = /^((?!class).)*$j/g;
-      // let re = '/^(?!.*(class)).*aderall';
-      const re = `\^\(\?\!\.\*\(class\)\)\.\*${badWord}`; // escaped string
-      // let re = `\/\^\(\?\!\.\*\(class\)\)\.\*${badWord}`;
-      // re = regexEscape(re);
-
+      const re = `\\b${badWord}\\b`;
       const theRegExp = new RegExp(re, 'gi');
-      checkedMessage = checkedMessage.replace(theRegExp, `<span class='bad-word'>${badWord}</span>`);
+      checkedMessage = checkedMessage.replace(theRegExp, `<span data-obscenity='bad-word'>${badWord}</span>`);
       return checkedMessage;
     }
   });
