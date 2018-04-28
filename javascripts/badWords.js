48686c31261342254701de594fe4c6ddec7c23c9
Drake Morrison <drakermorrison@gmail.com>
2018-04-28 13:56:52 -0500

    added css animations to bad words

--- javascripts/badWords.js
+++ javascripts/badWords.js
@@ -3,11 +3,11 @@ const data = require('./data.js');
 const checkBadWords = (checkedMessage) => {
   const badWords = data.getBadWords();
   badWords.forEach((badWord) => {
-    if (checkedMessage.includes(badWord)) {
+    if (checkedMessage.toLowerCase().includes(badWord)) {
       const re = `\\b${badWord}\\b`;
       const theRegExp = new RegExp(re, 'gi');
-      checkedMessage = checkedMessage.replace(theRegExp, `<span data-obscenity='bad-word'>${badWord}</span>`);
-      return checkedMessage;
+      // const userBadWord = findBadWord();
+      checkedMessage = checkedMessage.replace(theRegExp, `<span class='bad-word'>${badWord}</span>`);
     }
   });
   return checkedMessage;
